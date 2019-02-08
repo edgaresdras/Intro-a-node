7dc95d91faafab0cb0921ee7037d801bbc5d7839
esdrasgreco <esdras_greco@yahoo.com>
2019-02-07 21:31:09 -0600

    Pirate cow with Cowsay

---
+++ index.js
@@ -0,0 +1,10 @@
+var cowsay = require('cowsay');
+
+console.log(cowsay.say({
+    text : "Soy pirata",
+    e : "x-0",
+    T : "v "
+ }));   
+
+console.log('Hola');
+console.log('soy un archivo de js');
