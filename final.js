a5c99931374e6bd3410372d8d5e24fbca37df11f
MoisesAlVe <moalve7690@outlook.com>
2019-02-11 20:59:00 -0600

    añadido final.js e integradas nuevas variables en la historia

---
+++ final.js
@@ -0,0 +1,8 @@
+module.exports = {
+    story: function(personaje, villano, lugar, edad, hobby, nuevohobby) {
+        if(edad > 18 && edad < 80) {
+            return `Entonces cuando ${personaje} discutió con ${villano} mientras comían taquitos en ${lugar}, ${personaje} se dio cuenta que no era necesario que se dedicara a ${nuevohobby} porque siempre le había gustado ${hobby}.`
+        } else if (edad > 80) {
+            return `Pero ${villano} era más astuto y convenció a ${personaje} de dedicarse a ${hobby} porque no podía perder a su rival porque a éste le gustaba ahora ${nuevohobby}.`
+    }
+}
