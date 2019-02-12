2206846b79e0cc75f915adb833a3144dca16907d
MoisesAlVe <moalve7690@outlook.com>
2019-02-12 17:47:27 -0600

    añadido metido file system para incluir variable personajes de manera aleator ia

--- index.js
+++ index.js
@@ -1,22 +1,54 @@
 const cowsay = require('cowsay');
+const fs = require('fs');
 const intro = require('./intro');
 const nudo = require('./nudo');
 const final = require('./final');
 
-const personaje = "Moi";
 const edad = Math.round(Math.random()*100);
 const villano = "Diego";
 const lugar = "Centro historico";
 const hobby = "Andar en bici";
 const nuevohobby = "programar";
 
+fs.readFile('personajes.txt', 'UTF-8', function(err, personajes){
+    if (err) {
+        throw err;
+    } //validacion clasica
+
+    // (err) ? console.log(err) : console.log(personajes) //operador ternario como el if pero en una linea
+    
+    personajes = personajes.split(',');
+
+    let personajesRandom = Math.floor(Math.random() * personajes.length);
+    console.log(personajes[personajesRandom]);
+
+    let personaje = personajes[personajesRandom];
+    
+    //console.log(personajes);
+
+    console.log(intro.story(personaje, edad, villano, lugar, hobby, nuevohobby));
+    console.log(final.story(personaje, villano, lugar, edad, hobby, nuevohobby));
+
+    const introHistoria = intro.story(personaje, edad, villano, lugar, hobby, nuevohobby);
+    const finalHistoria = final.story(personaje, villano, lugar, edad, hobby, nuevohobby);
+
+    let fullHistoria = introHistoria + finalHistoria;
+
+
+    fs.writeFile('historia.txt', fullHistoria, function(err) {
+        if (err) {
+            throw err;
+        }
+    });
+
+});
+
+
 // let nombre = "Cowgirl";
 // let frase1 = "comiendo pasto electronico";
 // let frase2 = " y tal vez unos cuantos bytes";
 // let story = "Hola, mi nombre es " + nombre + ". Y hoy estare " + frase1 + frase2; 
 
-console.log(intro.story(personaje, edad, villano, lugar, hobby, nuevohobby));
-console.log(final.story(personaje, villano, lugar, edad, hobby, nuevohobby));
 
 console.log(cowsay.say({
     text : "Soy pirata",
