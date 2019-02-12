66da8ac843bb25793c0d4cfab7bc684ec983f5cc
MoisesAlVe <moalve7690@outlook.com>
2019-02-11 21:22:16 -0600

    corregido error en lineas 8 y 9 de final.js

--- final.js
+++ final.js
@@ -4,5 +4,6 @@ module.exports = {
             return `Entonces cuando ${personaje} discutió con ${villano} mientras comían taquitos en ${lugar}, ${personaje} se dio cuenta que no era necesario que se dedicara a ${nuevohobby} porque siempre le había gustado ${hobby}.`
         } else if (edad > 80) {
             return `Pero ${villano} era más astuto y convenció a ${personaje} de dedicarse a ${hobby} porque no podía perder a su rival porque a éste le gustaba ahora ${nuevohobby}.`
+        }
     }
 }
